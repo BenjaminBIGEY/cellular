fc611767063914952590f5a0b5e0e20b723876a3
BenjaminBIGEY <bigey@etud.insa-toulouse.fr>
2016-10-29 21:53:11 +0200

    CMakeLists.txt to use GLEW and GLFW

--- C++_3D/main.cpp
+++ C++_3D/main.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
-//#include "World/World.h"
+#include "World/World.h"
 
 // GLEW
 #define GLEW_STATIC
-//#include "World/Display/glew.h"
 #include <GL/glew.h>
 // GLFW
-//#include "World/Display/glfw3.h"
 #include <GLFW/glfw3.h>
 
 
 int main(int argc, char** argv) {
-    /*glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwInit();
+    /*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);*/
